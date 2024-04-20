353823abe125bead35b12948b487cfc689755f4d
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-04-20 12:30:43 -0300

    Add filter to show only one result by day

--- src/components/Forecast5Days/Forecast5Days.jsx
+++ src/components/Forecast5Days/Forecast5Days.jsx
@@ -4,13 +4,29 @@ import { WeatherContext } from '../../WeatherContext';
 const Forecast5Days = () => {
   const { forecast5Days, setForecast5Days } = useContext(WeatherContext);
 
+  function ignoreSameDay(element, index, array) {
+    if (index === 0) {
+      return true
+    } else {
+      if (element.dt_txt.split(' ')[0] === array[index - 1].dt_txt.split(' ')[0]) {
+        return false
+      }
+      else {
+        return true;
+      }
+    }
+  }
+
   return (
     <div className="flex gap-2">
       {forecast5Days &&
         forecast5Days.list &&
-        forecast5Days.list.map((element, index) => (
+        forecast5Days.list.filter((element, index, array) => ignoreSameDay(element, index, array)).map((element, index) => (
           <div key={index} className="w-1/5 h bg-cyan-950 rounded-xl p-5 mt-4">
+            <img src={`https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`} alt="Clima" />
+            <p>{element.main.temp} ºC</p>
             {element.weather[0].description}
+            {element.dt_txt}
           </div>
         ))}
     </div>
