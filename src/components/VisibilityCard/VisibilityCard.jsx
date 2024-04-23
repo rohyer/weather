bdfb5aa03d8f3229fbe874a5701c378f73622c60
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-04-23 19:54:41 -0300

    Develop VisibilityCard component

---
+++ src/components/VisibilityCard/VisibilityCard.jsx
@@ -0,0 +1,20 @@
+import CurrentDay from "../CurrentDay/CurrentDay";
+import { WeatherContext } from "../../WeatherContext";
+import { useContext } from "react";
+
+const VisibilityCard = () => {
+  const { currentWeather, setCurrentWeather } = useContext(WeatherContext);
+
+  return (
+    <div className="w-1/5 h bg-cyan-950 rounded-xl p-5 mt-4">
+      { currentWeather && currentWeather.weather && (
+        <>
+          <h4>Visibilidade</h4>
+          <p><span className="text-4xl">{currentWeather.visibility}</span>  metros</p> 
+        </>
+      )}
+    </div>
+  )
+}
+
+export default VisibilityCard;
