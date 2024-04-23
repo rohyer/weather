8eb73408602e28d31039b600330104ac2948952c
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-04-23 20:01:36 -0300

    Create CurrentDay component to agroup cards components

--- src/components/CurrentDay/CurrentDay.jsx
+++ src/components/CurrentDay/CurrentDay.jsx
@@ -1,39 +1,18 @@
 import { useContext } from 'react';
 import { WeatherContext } from '../../WeatherContext';
+import TempCard from '../TempCard/TempCard';
+import WindCard from '../WindCard/WindCard';
+import VisibilityCard from '../VisibilityCard/VisibilityCard';
+import SunCard from '../SunCard/SunCard';
 
 const CurrentDay = () => {
-  const { currentWeather, setCurrentWeather } = useContext(WeatherContext);
-
-  function getDate() {
-    const date = new Date();
-    const day = date.getDate();
-    const month = date.getMonth();
-    const year = date.getFullYear();
-
-    return (
-      <p>
-        {day}/{month}/{year}
-      </p>
-    );
-  }
 
   return (
-    <div className="w-1/5 h bg-cyan-950 rounded-xl p-5 mt-4">
-      {currentWeather && currentWeather.weather && (
-        <>
-          <img
-            src={`https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`}
-            alt="Clima"
-          />
-          <p className="text-5xl mb-3">{currentWeather.main.temp} ºC</p>
-          <p>{currentWeather.weather[0].description}</p>
-
-          <div className="flex items-center mt-2">
-            <i className="fa-solid fa-calendar-days mr-2"></i>
-            {getDate()}
-          </div>
-        </>
-      )}
+    <div className='flex gap-2'>
+      <TempCard />
+      <WindCard />
+      <VisibilityCard />
+      <SunCard />
     </div>
   );
 };
