f730ed559c8a7c15e3d71d2a1bf5a715a17fbf75
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-04-23 20:21:42 -0300

    Develop SunCard component

--- src/components/SunCard/SunCard.jsx
+++ src/components/SunCard/SunCard.jsx
@@ -5,17 +5,21 @@ const SunCard = () => {
   const { currentWeather, setCurrentWeather } = useContext(WeatherContext);
 
   function convertTime(timezone) {
-    return timezone
+    const date = new Date(timezone * 1000);
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    
+    return `${hour}:${minutes}`;
   }
 
   return (
-    <div className="w-1/5 h bg-cyan-950 rounded-xl p-5 mt-4">
+    <div className="flex flex-col justify-end w-1/5 h bg-cyan-950 rounded-xl p-5 mt-4">
       { currentWeather && (
         <>
-          <h4>Visibilidade</h4>
-          {convertTime(currentWeather.timezone)}
-          {/* <p>Nascer{currentWeather.sys.sunrise}</p> 
-          <p>Por{currentWeather.sys.sunset}</p>  */}
+          <h4>Nascer:</h4>
+          <p className="text-4xl">{convertTime(currentWeather.sys.sunrise)}</p>
+          <h4 className="mt-2">Pôr:</h4>
+          <p className="text-4xl">{convertTime(currentWeather.sys.sunset)}</p>
         </>
       )}
     </div>
