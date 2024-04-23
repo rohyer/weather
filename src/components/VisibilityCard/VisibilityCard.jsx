893cebcb15469f4e725083c52e99aeabf75239b2
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-04-23 20:22:19 -0300

    Align content to end

--- src/components/VisibilityCard/VisibilityCard.jsx
+++ src/components/VisibilityCard/VisibilityCard.jsx
@@ -6,7 +6,7 @@ const VisibilityCard = () => {
   const { currentWeather, setCurrentWeather } = useContext(WeatherContext);
 
   return (
-    <div className="w-1/5 h bg-cyan-950 rounded-xl p-5 mt-4">
+    <div className="flex flex-col justify-end w-1/5 h bg-cyan-950 rounded-xl p-5 mt-4">
       { currentWeather && currentWeather.weather && (
         <>
           <h4>Visibilidade</h4>
