97a08433efb18f88be5f13c4cce4a49a291b1a62
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-04-29 20:31:00 -0300

    Remove components

--- src/components/CurrentDay/CurrentDay.jsx
+++ src/components/CurrentDay/CurrentDay.jsx
@@ -1,8 +1,6 @@
 import { useContext } from 'react';
 import { WeatherContext } from '../../WeatherContext';
 import TempCard from '../TempCard/TempCard';
-import WindCard from '../WindCard/WindCard';
-import VisibilityCard from '../VisibilityCard/VisibilityCard';
 import SunCard from '../SunCard/SunCard';
 
 const CurrentDay = () => {
@@ -10,8 +8,6 @@ const CurrentDay = () => {
   return (
     <div className='flex gap-2'>
       <TempCard />
-      <WindCard />
-      <VisibilityCard />
       <SunCard />
     </div>
   );
