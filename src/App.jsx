b4978509a7b14b8421d5d808847f8af900c758fd
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-04-29 20:31:44 -0300

    Import Cities components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import { WeatherContext } from './WeatherContext';
 import CurrentDay from './components/CurrentDay/CurrentDay';
 import Forecast5Days from './components/Forecast5Days/Forecast5Days';
+import Cities from './components/Cities/Cities';
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState();
@@ -16,10 +17,13 @@ function App() {
   function getComponents() {
     if (currentWeather && currentWeather.cod === 200) {
       return (
-        <>
-          <CurrentDay />
-          <Forecast5Days />
-        </>
+        <div className='flex flex-wrap gap-2'>
+          <div className='w-full lg:w-4/6'>
+            <CurrentDay />
+            <Forecast5Days />
+          </div>
+          <Cities />
+        </div>
       );
     }
   }
