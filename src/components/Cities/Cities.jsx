05eee3a709ebcbaad5b0708f8db03590d50d3406
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-07-17 22:51:28 -0300

    Add key to list

--- src/components/Cities/Cities.jsx
+++ src/components/Cities/Cities.jsx
@@ -5,39 +5,45 @@ const Cities = () => {
 
   useEffect(() => {
     async function fetchCities(lat, lon) {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2485fa7ddb71d3d0fe506f3dc5d8d4eb&lang=pt_br&units=metric`);
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2485fa7ddb71d3d0fe506f3dc5d8d4eb&lang=pt_br&units=metric`
+      );
       const json = await response.json();
       setCities((prev) => {
-        return [...prev, json]
-      })
-      console.log(json.name)
+        return [...prev, json];
+      });
+      // console.log(json.name);
     }
 
-    fetchCities(51.5085300, -0.1257400);
-    fetchCities(35.6895000, 139.6917100);
-    fetchCities(40.7142700, -74.0059700);
-    fetchCities(-23.5475000, -46.6361100);
+    fetchCities(51.50853, -0.12574);
+    fetchCities(35.6895, 139.69171);
+    fetchCities(40.71427, -74.00597);
+    fetchCities(-23.5475, -46.63611);
   }, []);
-
-  cities &&
-  console.log(cities);
 
   return (
     <div className="flex flex-col justify-start w-1/5 h gap-2 mt-4">
       {cities.map((city) => (
-        <div className="flex flex-row justify-between items-center w-full h bg-cyan-950 rounded-xl p-5">
+        <div
+          key={city.id}
+          className="flex flex-row justify-between items-center w-full h bg-cyan-950 rounded-xl p-5"
+        >
           <div>
             <h3>{city.name}</h3>
             <p>{city.weather[0].description}</p>
           </div>
           <div>
-            <img className="w-14 h-auto" src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`} alt="Clima" />
+            <img
+              className="w-14 h-auto"
+              src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
+              alt="Clima"
+            />
             <p>{city.main.temp} ºC</p>
           </div>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default Cities;
